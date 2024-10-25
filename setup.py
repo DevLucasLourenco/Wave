ac53d31132cd9799106a900c36af392caf595bad
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2024-10-25 13:04:58 -0300

    updates setup

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open(r"README.md", "r", encoding='utf-8') as arq:
+with open("README.md", "r", encoding="utf-8") as arq:
     readme = arq.read()
 
-setup(name='Wave',
-    version='1.0.0',
-    license='MIT License',
-    author='Lucas Lourenço',
+setup(
+    name="wave-flow",
+    version="1.0.0",
+    license="MIT License",
+    author="Lucas Lourenço",
     long_description=readme,
     long_description_content_type="text/markdown",
-    author_email='dev.lucaslourenco@gmail.com',
-    keywords='wave build docx treatdata xlsx to word',
-    description=u'Wave - Workflow Automation and Versatile Engine',
-    packages=['PreRequisitesWave','To','DataHandler','Builder','Transmitter'],
-    install_requires=['python-docx','openpyxl','pandas'],)
+    author_email="dev.lucaslourenco@gmail.com",
+    keywords="wave build docx treatdata xlsx to word",
+    description="Wave - Workflow Automation and Versatile Engine",
+    packages=find_packages(),
+    install_requires=["python-docx", "openpyxl", "pandas"],
+)
