f6dd39816ee1bc435bf145b01db8bb51226cc5b1
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2024-12-03 16:37:14 -0300

    uploads setup's version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as arq:
 
 setup(
     name="wave-flow",
-    version="1.0.7",
+    version="1.0.8",
     license="MIT License",
     author="Lucas Lourenço",
     long_description=readme,
